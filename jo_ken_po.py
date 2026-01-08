17110177e06718eed38f0b383e0194ba6badb616
jefl7 <lealjoaoeduardo@gmail.com>
2026-01-08 18:16:57 -0300

    Add files via upload
    I made some changes in the code, adding a simple layout and, a timer in the commands and the player name. Change the name of Computer to "The turk", the first "AI" in the world.

--- jo_ken_po.py
+++ jo_ken_po.py
@@ -2,7 +2,13 @@ from random import randint
 import time
 options = ('Rock', 'Paper', 'Scissors')
 computer = randint(0,2)
-name = str(input('Enter your name:'))
+print('My name is The Turk, the master of Jo Ken Po')
+time.sleep(2)
+print('After leaving the chess behind, I decided to try a new game...')
+time.sleep(2)
+print('Do you have the courage to challenge me?')
+time.sleep(2)
+name = str(input('If you have, so enter your name:'))
 print('<=>'* 20)
 print('Welcome to Jo Ken Po game {}'.format(name).center(60))
 print('<=>'* 20)
@@ -10,22 +16,25 @@ player = int(input('''Chose your option:
 0.Rock
 1.Paper
 2.Scissors\n'''))
-print('Jo')
-time.sleep(1)
-print('Ken')
-time.sleep(1)
-print('Po')
-time.sleep(1)
+
+if  player < 0 or player > 2:
+    print('Invalid moviment')
+else:
+    print('Jo')
+    time.sleep(1)
+    print('Ken')
+    time.sleep(1)
+    print('Po')
+    time.sleep(1)
 
 print('<=>'* 20)
-print('The computer chose {}'.format(options[computer]))
-print('The player chose {}'.format(options[player]))
+print('The Turk chose {}'.format(options[computer]).center(60))
+print('{} chose {}'.format(name, options[player]).center(60))
 print('<=>'* 20)
-if 0 > player or player > 2:
-    print('Invalid moviment')
-elif player == computer:
+
+if player == computer:
     print('We have a draw!')
-elif player == 0 and computer == 1 or player == 1 and computer == 2 or player == 1 and computer == 0:
+elif player == 0 and computer == 1 or player == 1 and computer == 2 or player == 2 and computer == 0:
     print('You lose!')
-else:
+elif player == 0 and computer == 2 or player == 1 and computer == 0 or player == 2 and computer == 1:
     print('You win!')
